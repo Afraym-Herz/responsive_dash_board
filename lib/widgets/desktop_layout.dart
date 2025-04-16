2bdcec39ba581ab9c31921d8956f272570b8581d
Afraym-Herz <afraym100herz@gmail.com>
2025-04-16 20:44:42 +0200

    create dots indicator

--- lib/widgets/desktop_layout.dart
+++ lib/widgets/desktop_layout.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/all_expenses.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
+import 'package:responsive_dash_board/widgets/my_card.dart';
+import 'package:responsive_dash_board/widgets/my_card_page_view.dart';
+import 'package:responsive_dash_board/widgets/my_card_section.dart';
 import 'package:responsive_dash_board/widgets/quick_invoice.dart';
 
 class DesktopLayout extends StatelessWidget {
@@ -12,7 +15,8 @@ class DesktopLayout extends StatelessWidget {
       children: [
         Expanded(child: CustomDrawer() , ),
         SizedBox(width: 24,) , 
-        Expanded(flex: 3,child: SingleChildScrollView(
+        Expanded(flex: 3,
+        child: SingleChildScrollView(
           child: Column(
             children: [
               AllExpenses(),
@@ -21,7 +25,8 @@ class DesktopLayout extends StatelessWidget {
             ],
           ),
         ) ,),
-        Expanded(flex: 2,child: Placeholder() ,),
+        SizedBox(width: 24,),
+        Expanded(flex: 2, child: MyCardSection(), ),
       ],
     );
   }
